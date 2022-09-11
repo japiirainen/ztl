1326caae1c928cb74a1a4c9df5b1ef0f0904ee9f
Joona Piirainen <joona.piirainen@gmail.com>
2022-09-11 11:51:07 +0300

    Test: add tests for project YAML block

--- src/ZtlMark.hs
+++ src/ZtlMark.hs
@@ -4,8 +4,14 @@ module ZtlMark (
   parse,
   Extension,
   render,
+  projectYaml,
 ) where
 
+import Data.Aeson
 import ZtlMark.Parser (ZtlMarkError (..), parse)
 import ZtlMark.Render (render)
 import ZtlMark.Type
+
+-- | Extract contents of an optional YAML block that may have been parsed.
+projectYaml :: ZtlMark -> Maybe Value
+projectYaml = ztlMarkYaml
--- tests/ZtlMarkSpec.hs
+++ tests/ZtlMarkSpec.hs
@@ -1,7 +1,11 @@
 module ZtlMarkSpec (spec) where
 
+import Data.Aeson
 import Test.Hspec
+import Test.Hspec.Megaparsec
 import TestUtils
+import Text.Megaparsec (ErrorFancy (..), Stream)
+import ZtlMark
 
 spec :: Spec
 spec = parallel $ do
@@ -24,3 +28,44 @@ spec = parallel $ do
           ==-> "<p>aaa\nbbb\nccc</p>\n"
       it "CM187" $
         "   aaa\nbbb" ==-> "<p>aaa\nbbb</p>\n"
+
+  describe "YAML block" $ do
+    context "when doc does not contain a YAML section" $
+      it "returns Nothing" $ do
+        doc <- mkDoc "No YAML block."
+        ZtlMark.projectYaml doc `shouldBe` Nothing
+
+    context "when document contains a YAML block" $ do
+      let expected =
+            object
+              [ "x" .= Number 69
+              , "y" .= Number 420
+              ]
+      it "returns the YAML section (1)" $ do
+        doc <- mkDoc "---\nx: 69\ny: 420\n---\nHere we go."
+        ZtlMark.projectYaml doc `shouldBe` Just expected
+      it "returns the YAML section (2)" $ do
+        doc <- mkDoc "---\nx: 69\ny: 420\n---\n\n"
+        ZtlMark.projectYaml doc `shouldBe` Just expected
+
+    context "when it is invalid" $ do
+      let mappingError =
+            fancy . ErrorCustom . YamlParseError $
+              "mapping values are not allowed in this context"
+      it "signals correct parse error" $
+        let s = "---\nx: 100\ny: x:\n---\nHere we go."
+         in s ~-> errFancy 15 mappingError
+      it "does not choke and can report more parse errors" $
+        let s = "---\nx: 100\ny: x:\n---\nHere we *go."
+         in s
+              ~~-> [ errFancy 15 mappingError
+                   , err 33 (ueib <> etok '*' <> eic)
+                   ]
+
+-- | Unexpected end of inline block.
+ueib :: Stream s => ET s
+ueib = ulabel "end of inline block"
+
+-- | Expecting inline content.
+eic :: Stream s => ET s
+eic = elabel "inline content"
